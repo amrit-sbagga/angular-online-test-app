b936e5edf2a7eb15b14b27d6df909c9a3f5b161f
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-16 01:00:18 +0530

    added option for selecting answer

--- src/app/components/questions-home/questions-home.component.ts
+++ src/app/components/questions-home/questions-home.component.ts
@@ -9,6 +9,10 @@ import { TestService } from '../../services/test.service';
 })
 export class QuestionsHomeComponent implements OnInit {
 
+  radioSelected:string;
+  radioSelectedString:string;
+  radioSel:any = '';
+
   questionsList
   constructor(private testService : TestService) { }
 
@@ -19,6 +23,32 @@ export class QuestionsHomeComponent implements OnInit {
       this.questionsList = data;
       //loadQuestions(data))
     })
+  }
+
+  //https://www.freakyjolly.com/how-to-show-radio-input-listing-in-angular-6/#.YAHjKugzZPY
+
+  getSelecteditem(queindex, optionindex){
+    console.log("selected index is = ", queindex, "-", optionindex);
+    //console.log(this.questionsList[queindex].options);
+    this.radioSelected = this.questionsList[queindex].options[optionindex].optid;
+    console.log("radioSelected = ", this.radioSelected);
+   
+    //todo - check undefined for radiosel
+    this.radioSel = this.questionsList[queindex].options.find(Item => {
+      console.log("Item =", Item)
+      console.log("Item radioSelected=", this.radioSelected)
+      Item.optid === this.radioSelected
+    });
+
+    console.log("radioSel = ", this.radioSel);
+    this.radioSelectedString = JSON.stringify(this.radioSel);
+    
+    console.log("radioSelectedString = ", this.radioSelectedString);
+  }
+
+  onItemChange(opItem, queindex, optionindex){
+    console.log("onItemChange called with opItem = ", opItem);
+    this.getSelecteditem(queindex, optionindex);
   }
 
   submitTest(){
