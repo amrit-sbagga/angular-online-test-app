f23e569e0d46bb8b527890988ab35e7bcbdbc260
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-19 01:25:30 +0530

    popup model added for starting exam

--- src/app/components/user-home/user-home.component.ts
+++ src/app/components/user-home/user-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-home',
@@ -9,10 +10,37 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class UserHomeComponent implements OnInit {
 
-  constructor(private route : Router, private toastr: ToastrService) { }
+  closeResult = ''; 
+
+  constructor(private route : Router, private toastr: ToastrService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+
+  open(content) { 
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => { 
+      this.closeResult = `Closed with: ${result}`; 
+      console.log(result)
+      if(result === "Save click"){
+        console.log("navigate hereeee to exam....");
+        this.route.navigate(['/questionshome']);
+      }
+    }, (reason) => { 
+      this.closeResult =  
+         `Dismissed ${this.getDismissReason(reason)}`; 
+         console.log("dismiss = ", this.closeResult)
+    }); 
+  } 
+  
+  private getDismissReason(reason: any): string { 
+    if (reason === ModalDismissReasons.ESC) { 
+      return 'by pressing ESC'; 
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) { 
+      return 'by clicking on a backdrop'; 
+    } else { 
+      return `with: ${reason}`; 
+    } 
+  } 
 
   enterExam(subject){
     console.log("subject = ", subject);
