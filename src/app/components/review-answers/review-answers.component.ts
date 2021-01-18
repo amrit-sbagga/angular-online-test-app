9c4afb0d069912057005df8fa2198c058ffc0e6a
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-18 21:46:02 +0530

    Added review answer component

---
+++ src/app/components/review-answers/review-answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-answers',
+  templateUrl: './review-answers.component.html',
+  styleUrls: ['./review-answers.component.scss']
+})
+export class ReviewAnswersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
