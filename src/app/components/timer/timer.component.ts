10977366338e274189eb55d2c9f915935bce4d01
Amrit Singh <amrit.sbagga@gmail.com>
2021-02-20 00:37:28 +0530

    implemented timer

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
@@ -7,12 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  myTimerSubscription: Subscription;
+  minutes = 9;
+  seconds = 60;
+
+  minutesStr;
+  secondsStr;
+
   constructor() { }
 
-  minutes = "10"
-  seconds = "00"
-
   ngOnInit(): void {
-  }
+
+      //timer called every second
+      this.myTimerSubscription = interval(1000).subscribe(x => {
+        this.timerFunction();
+      });
+      
+   }
+
+   timerFunction(){
+      this.seconds = this.seconds - 1;
+      if(this.seconds == 0){
+        this.seconds = 60;
+        this.minutes = (this.minutes - 1);
+        if(this.minutes <= -1){
+          this.seconds = 0;
+          this.minutes = 0;
+          this.myTimerSubscription.unsubscribe();
+        }
+      }
+
+      this.minutesStr = (this.minutes < 10 ) ? "0" + this.minutes.toString() : this.minutes.toString();
+      this.secondsStr = (this.seconds < 10 ) ? "0" + this.seconds.toString() : this.seconds.toString();
+      
+   }
 
 }
